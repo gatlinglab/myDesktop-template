f6b582c43b6bdfe8d9217707ccd65f87bbd51076
venividivici66 <venividivici@dmc.chat>
2024-06-26 22:05:36 -0400

    Create prefs.js

---
+++ userdata/prefs.js
@@ -0,0 +1,4 @@
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.disk.max_entry_size", 512);
+user_pref("browser.download.dir", "/workspaces/myDesktop/download");
+user_pref("media.peerconnection.enabled", false);

2b38022e2d2a5613b8f952d9c785428a0944ff4c
cmworko <166978556+cmworko@users.noreply.github.com>
2024-06-29 15:38:47 -0700

    Update prefs.js

--- userdata/prefs.js
+++ userdata/prefs.js
@@ -2,5 +2,5 @@ user_pref("browser.cache.disk.enable", false);
 user_pref("browser.cache.disk.max_entry_size", 512);
 user_pref("browser.download.dir", "/workspaces/myDesktop/download");
 user_pref("media.peerconnection.enabled", false);
-user_pref("browser.cache.disk.capacity", 1280);
+user_pref("browser.cache.disk.capacity", 512);
 user_pref("layout.spellcheckDefault", 0);
